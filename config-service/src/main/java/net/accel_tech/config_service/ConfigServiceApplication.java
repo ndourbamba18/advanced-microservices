f75f4871bd59ecd2576818acc366df816478a442
ndourbamba18 <ndourbamba18@gmail.com>
2026-01-01 14:43:12 +0000

    Spring Cloud Config Server

--- config-service/src/main/java/net/accel_tech/config_service/ConfigServiceApplication.java
+++ config-service/src/main/java/net/accel_tech/config_service/ConfigServiceApplication.java
@@ -9,7 +9,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ConfigServiceApplication.class, args);
+
+		System.out.println("Config Service started successfully...");
 	}
 
 }
