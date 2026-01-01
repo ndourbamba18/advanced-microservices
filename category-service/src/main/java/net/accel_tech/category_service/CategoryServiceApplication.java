ee3f0a48b8b19b64c664d34b1e344742205afeb1
ndourbamba18 <ndourbamba18@gmail.com>
2026-01-01 16:25:18 +0000

    Spring Cloud Eureka Server

--- category-service/src/main/java/net/accel_tech/category_service/CategoryServiceApplication.java
+++ category-service/src/main/java/net/accel_tech/category_service/CategoryServiceApplication.java
@@ -2,9 +2,11 @@ package net.accel_tech.category_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableJpaAuditing
 public class CategoryServiceApplication {
 
