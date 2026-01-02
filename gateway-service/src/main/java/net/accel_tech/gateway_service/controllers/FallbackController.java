b8dfe428476ddf80baa4e567d5e7ba152283d82b
ndourbamba18 <ndourbamba18@gmail.com>
2026-01-02 22:14:31 +0000

    Integration of Circuit Breaker

--- gateway-service/src/main/java/net/accel_tech/gateway_service/controllers/FallbackController.java
+++ gateway-service/src/main/java/net/accel_tech/gateway_service/controllers/FallbackController.java
@@ -1,21 +1,33 @@
 package net.accel_tech.gateway_service.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/fallback")
 public class FallbackController {
 
-    @GetMapping("/product-service")
-    public Mono<String> productServiceFallback() {
-        return Mono.just("Le service produit est temporairement indisponible. Veuillez réessayer plus tard.");
+    @RequestMapping("/category-service")
+    public Mono<ResponseEntity<Map<String, String>>> categoryServiceFallback() {
+        Map<String, String> response = new HashMap<>();
+        //response.put("message", "Service indisponible");
+        response.put("message", "Le service catégorie ne répond pas. Maintenance en cours.");
+        response.put("status", "error");
+        return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response));
     }
 
-    @GetMapping("/category-service")
-    public Mono<String> categoryServiceFallback() {
-        return Mono.just("Le service catégorie ne répond pas. Maintenance en cours.");
+    @RequestMapping("/product-service")
+    public Mono<ResponseEntity<Map<String, String>>> productServiceFallback() {
+        Map<String, String> response = new HashMap<>();
+        //response.put("message", "Service indisponible");
+        response.put("message", "Le service produit est temporairement indisponible. Veuillez réessayer plus tard.");
+        response.put("status", "error");
+        return Mono.just(ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(response));
     }
 }
